22b0e51187f3e84b6db3610a288fd6b2f53f1fbf
ParkedHampster <90224252+ParkedHampster@users.noreply.github.com>
2023-02-27 23:15:57 -0500

    checkpoint 2 on the blog upload to make sure that things exist

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230227.223237';
+const cacheName = 'chirpy-20230227.231437';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
