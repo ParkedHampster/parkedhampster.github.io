4eeb62afdf6dbc550de076eae3fc43759eb2870a
ParkedHampster <90224252+ParkedHampster@users.noreply.github.com>
2023-02-27 22:37:20 -0500

    trying again to push up

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230227.213304';
+const cacheName = 'chirpy-20230227.223237';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
